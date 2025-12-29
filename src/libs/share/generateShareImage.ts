0287dfd7b4b3afa8049c7b6380d46c65f15a84b8
yongholeeme <yongholeeme@gmail.com>
2025-12-29 14:58:44 +0900

    ♻️ Refactor formatNumber function: Remove K notation for numbers over 1000 to simplify number formatting

--- src/libs/share/generateShareImage.ts
+++ src/libs/share/generateShareImage.ts
@@ -43,9 +43,6 @@ async function loadImage(src: string): Promise<HTMLImageElement> {
 }
 
 function formatNumber(num: number): string {
-    if (num >= 1000) {
-        return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K'
-    }
     return num.toLocaleString()
 }
 
